e53a5aca995ed5d3d1c43f57354955cc96c308c4
Joseph Groocock <frebib@gmail.com>
2015-11-21 03:27:14 +0000

    Added WebPage parser to get all anchor/img tags

---
+++ src/net/frebib/sscdownloader/WebpageParser.java
@@ -0,0 +1,62 @@
+package net.frebib.sscdownloader;
+
+import org.jsoup.Connection;
+import org.jsoup.helper.HttpConnection;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class WebpageParser {
+    public static List<URL> parse(URL location, LinkType type, int timeoutMillis) throws IOException {
+        Connection con = HttpConnection.connect(location);
+        con.timeout(timeoutMillis);
+        return parse(con.get(), type);
+    }
+
+    public static List<URL> parse(Document doc, LinkType type) {
+        Elements els = new Elements();
+        if (type == LinkType.Anchor || type == LinkType.Both)
+            els.addAll(doc.getElementsByTag("a"));
+        if (type == LinkType.Image || type == LinkType.Both)
+            els.addAll(doc.getElementsByTag("img"));
+
+        return els.stream()
+                .map(e -> {
+                    // Use `abs:` prefix to attributes to get absolute path
+                    if (e.tag().getName().equalsIgnoreCase("a"))
+                        return e.attr("abs:href");
+                    else if (e.tag().getName().equalsIgnoreCase("img"))
+                        return e.attr("abs:src");
+                    return null;
+                })
+                .map(s -> toURL(s, doc.location()))
+                .filter(url -> url != null)
+                .collect(Collectors.toList());
+    }
+
+    private static URL toURL(String address, String parent) {
+        try {
+            if (address == null) return null;
+            if (address.startsWith("//"))                   // Add missing protocol
+                address = parent.split("//")[0] + address;  // if link is relative
+
+            DownloaderClient.LOG.finest(address);
+
+            return new URL(address);
+        } catch (MalformedURLException e) {
+            DownloaderClient.LOG.severe("Address is not valid: " + address);
+        }
+        return null;
+    }
+
+    public enum LinkType {
+        Anchor,
+        Image,
+        Both
+    }
+}

307103dda34a8997517905ebb70752efffb99d5a
Joseph Groocock <frebib@gmail.com>
2015-11-24 18:29:23 +0000

    Already done TODO

--- src/net/frebib/sscdownloader/WebpageCrawler.java
+++ src/net/frebib/sscdownloader/WebpageCrawler.java
@@ -25,8 +25,6 @@ public class WebpageCrawler {
         if (type == LinkType.Image || type == LinkType.Both)
             els.addAll(doc.getElementsByTag("img"));
 
-        // TODO: Remove duplicate links
-
         return els.stream()
                 .map(e -> {
                     // Use `abs:` prefix to attributes to get absolute path

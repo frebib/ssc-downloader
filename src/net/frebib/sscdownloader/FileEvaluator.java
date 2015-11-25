2f634ed2bc343d2d4becb90772d47493589a4360
Joseph Groocock <frebib@gmail.com>
2015-11-25 21:08:57 +0000

    EvalTasks can now be re-evaluated

--- src/net/frebib/sscdownloader/FileEvaluator.java
+++ src/net/frebib/sscdownloader/FileEvaluator.java
@@ -7,25 +7,27 @@ import net.frebib.sscdownloader.concurrent.Task;
 import java.io.File;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class FileEvaluator {
-    private final BatchExecutor<Task<URL, DownloadTask>, DownloadTask> executor;
+    private final BatchExecutor<EvalTask, DownloadTask> executor;
     private final MimeTypeCollection mimeTypes;
 
+    private final List<EvalTask> evalTasks;
     private final List<DownloadTask> results;
 
     public FileEvaluator(MimeTypeCollection mimes, int threadCount, Completion<List<DownloadTask>> done) {
         executor = new BatchExecutor<>(threadCount);
         executor.done(done);
         results = new ArrayList<>();
+        evalTasks = new ArrayList<>();
         mimeTypes = mimes;
     }
 
     public FileEvaluator add(URL url, File directory, Completion<DownloadTask> done) {
-        executor.add(new EvalTask(url, directory).done(done));
+        EvalTask task = (EvalTask) new EvalTask(url, directory).done(done);
+        executor.add(task);
+        evalTasks.add(task);
         return this;
     }
 
@@ -40,7 +42,11 @@ public class FileEvaluator {
     }
 
     private class EvalTask extends Task<URL, DownloadTask> {
+        private URL url;
         private File directory;
+        private String mimeString, filename;
+
+        private boolean fetched;
 
         public EvalTask(URL url, File directory) {
             super(url);
@@ -55,42 +61,53 @@ public class FileEvaluator {
                 conn.setRequestMethod("HEAD");
                 conn.connect();
 
+                fetched = true;
+
                 // Test file extension
-                String mimeStr = conn.getContentType();
-                File f = new File(conn.getURL().getFile());
-                String filename = f.getCanonicalFile().getName();
+                File f = new File(url.getFile());
+                this.url = conn.getURL();
+                filename = f.getCanonicalFile().getName();
+                mimeString = conn.getContentType();
 
-                String ext = "";
-                int index = 0;
-                index = filename.lastIndexOf('.');
-                if (index > 0) {
-                    ext = filename.substring(index + 1);
-                    filename = filename.substring(0, index);
-                }
-
-                // Check if mime type is valid
-                if (mimeTypes.hasMime(mimeStr)) {
-                    // Use the default extension if it's invalid
-                    if (!mimeTypes.hasExtension(ext)) {
-                        MimeType type = mimeTypes.getMimeType(mimeStr);
-                        if (type != null)
-                            ext = Optional.ofNullable(type.getDefaultExtension()).orElse("");
-                    }
-
-                    if (ext.isEmpty())     // Otherwise have no extension
-                        DownloaderClient.LOG.warning("File \"" + filename + "\" has no extension with mime: " + mimeStr);
-                } else
-                    return null;    // Return null if the mime type isn't valid
-
-                // Append new extension && remove invalid chars
-                if (!ext.isEmpty())
-                    filename = filename.replaceAll("[^a-zA-Z0-9.-]", "_") + '.' + ext;
-
-                return new DownloadTask(url, filename, directory);
+                return evalExtension();
             } catch (Exception e) {
                 DownloaderClient.LOG.exception(e);
             }
             return null;
+        }
+
+        private DownloadTask evalExtension() throws IllegalStateException {
+            if (!fetched)
+                throw new IllegalStateException("Cannot evaluate file, it has not been fetched");
+
+            String ext = "";
+            int index = 0;
+            index = filename.lastIndexOf('.');
+            if (index > 0) {
+                ext = filename.substring(index + 1);
+                filename = filename.substring(0, index);
+            }
+
+            // TODO: Check globally against all MimeTypes
+            // Check if mime type is valid
+            if (mimeTypes.hasMime(mimeString)) {
+                // Use the default extension if it's invalid
+                if (!mimeTypes.hasExtension(ext)) {
+                    MimeType type = mimeTypes.getMimeType(mimeString);
+                    if (type != null)
+                        ext = Optional.ofNullable(type.getDefaultExtension()).orElse("");
+                }
+
+                if (ext.isEmpty())     // Otherwise have no extension
+                    DownloaderClient.LOG.warning("File \"" + filename + "\" has no extension with mime: " + mimeString);
+            } else
+                return null;    // Return null if the mime type isn't valid
+
+            // Append new extension && remove invalid chars
+            if (!ext.isEmpty())
+                filename = filename.replaceAll("[^a-zA-Z0-9.-]", "_") + '.' + ext;
+
+            return new DownloadTask(url, filename, directory);
         }
     }
 }

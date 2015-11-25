7ea950b3fd0d365bf6a4c3d4dce0c247aa72f3e2
Joseph Groocock <frebib@gmail.com>
2015-11-25 02:06:23 +0000

    Add right-click menu to downloads

--- src/net/frebib/sscdownloader/gui/DownloadList.java
+++ src/net/frebib/sscdownloader/gui/DownloadList.java
@@ -5,24 +5,56 @@ import net.frebib.sscdownloader.DownloadTask;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
-public class DownloadList extends JList<DownloadTask> implements MouseListener, KeyListener {
-
+public class DownloadList extends JList<DownloadTask> implements KeyListener {
     public DownloadList(ListModel<DownloadTask> model) {
         super(model);
 
-        addMouseListener(this);
         addKeyListener(this);
+        addMouseListener(new RightClickHandler(this));
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.isPopupTrigger()) {
-            // TODO: Trigger right click menu
-        }
+    private JPopupMenu initMenu(DownloadTask task) {
+        JPopupMenu menu = new JPopupMenu();
+        menu.removeAll();
+
+        JMenuItem mi = new JMenuItem(task.getFilename());
+        mi.setEnabled(false);
+        menu.add(mi);
+
+        mi = new JMenuItem(String.format("%.1f", task.getProgress()) + "% Complete");
+        mi.setEnabled(false);
+        menu.add(mi);
+
+        mi = new JMenuItem(humanReadableBytes(task.getBytes(), false) + " / " +
+                           humanReadableBytes(task.getSize(), false));
+        mi.setEnabled(false);
+        menu.add(mi);
+
+        mi = new JMenuItem(humanReadableBytes(task.getSize(), false));
+        mi.setEnabled(false);
+        menu.add(mi);
+
+        menu.addSeparator();
+
+        mi = new JMenuItem("Cancel", UIManager.getIcon("InternalFrame.closeIcon"));
+        mi.addActionListener(e -> task.cancel());
+        menu.add(mi);
+
+        return menu;
     }
+
+    // Courtesy of  http://stackoverflow.com/questions/3758606/
+    public static String humanReadableBytes(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_DELETE) {
@@ -31,10 +63,24 @@ public class DownloadList extends JList<DownloadTask> implements MouseListener, 
                 dl.cancel();
         }
     }
-    public void mousePressed(MouseEvent e) { }
-    public void mouseReleased(MouseEvent e) { }
-    public void mouseEntered(MouseEvent e) { }
-    public void mouseExited(MouseEvent e) { }
     public void keyPressed(KeyEvent e) { }
     public void keyReleased(KeyEvent e) { }
+
+    private class RightClickHandler extends MouseAdapter {
+        private DownloadList list;
+
+        public RightClickHandler(DownloadList list) {
+            this.list = list;
+        }
+
+        public void onMouseEvent(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                DownloadTask mt = getModel().getElementAt(locationToIndex(e.getPoint()));
+                setSelectedValue(mt, false);
+                initMenu(mt).show(list, e.getX(), e.getY());
+            }
+        }
+        public void mousePressed(MouseEvent e) { onMouseEvent(e); }
+        public void mouseReleased(MouseEvent e) { onMouseEvent(e); }
+    }
 }
